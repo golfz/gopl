51fe46373207b6ad983956cadd3f94e9b85700e3
surattikorn <surattikorn@gmail.com>
2023-02-07 05:51:41 +0700

    build: add go.mod

--- ch8/netcat1/main.go
+++ ch8/netcat1/main.go
@@ -1,1 +1,25 @@
-package netcat1
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	mustCopy(os.Stdout, conn)
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	fmt.Println("mustCopy")
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/golfz/gopl
+
+go 1.20
+
+require github.com/golfz/golify v1.4.0
